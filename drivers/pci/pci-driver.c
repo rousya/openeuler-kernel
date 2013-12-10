8d6512347ca61201bd32f37994672cc24a7c1e69
Yijing Wang <wangyijing@huawei.com>
2013-12-10 18:31:34 +0800

    PCI: Don't try to disable Bus Master on disconnected PCI devices
    mainline inclusion
    from mainline-3.9
    upstream commit 6e0eda3c389887168455b92b6f7b48f552227067
    category: bugfix
    
    --------------------------------------------------------
    
    This is a fix for commit 7897e60227 ("PCI: Disable Bus Master
    unconditionally in pci_device_shutdown()").  Vivek reported that
    with this commit, kexec failed because none of his SATA disks
    came up.
    
    A ->shutdown() callback might put the device in D3cold, which means config
    space is no longer available.
    
    [bhelgaas: changelog]
    Link: https://lkml.org/lkml/2013/3/12/529
    Reported-and-Tested-by: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    [context adjust]
    Integrated-by: Yijing Wang <wangyijing@huawei.com>

--- drivers/pci/pci-driver.c
+++ drivers/pci/pci-driver.c
@@ -422,9 +422,10 @@ static void pci_device_shutdown(struct device *dev)
 
 	/*
 	 * Turn off Bus Master bit on the device to tell it to not
-	 * continue to do DMA
+	 * continue to do DMA. Don't touch devices in D3cold or unknown states.
 	 */
-	pci_clear_master(pci_dev);
+	if (pci_dev->current_state <= PCI_D3hot)
+		pci_clear_master(pci_dev);
 
 	/*
 	 * Devices may be enabled to wake up by runtime PM, but they need not
