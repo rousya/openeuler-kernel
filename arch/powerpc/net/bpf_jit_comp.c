75aba5c656cc1230f07921c37abea943b6c19b53
Rusty Russell <rusty@rustcorp.com.au>
2015-01-20 09:07:05 +1030

    module: remove mod arg from module_free, rename module_memfree().
    Mainline inclusion
    from:  Linux 3.19
    target:    kernel 3.4
    category:  Bugfix/Bug 2048
    
    -------------------------------
    
    upstream d453cded05ee219b77815ea194dc36efa5398bca commit.
    
    Nothing needs the module pointer any more, and the next patch will
    call it from RCU, where the module itself might no longer exist.
    Removing the arg is the safest approach.
    
    This just codifies the use of the module_alloc/module_free pattern
    which ftrace and bpf use.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Cc: Mikael Starvik <starvik@axis.com>
    Cc: Jesper Nilsson <jesper.nilsson@axis.com>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Ley Foon Tan <lftan@altera.com>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Chris Metcalf <cmetcalf@ezchip.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: x86@kernel.org
    Cc: Ananth N Mavinakayanahalli <ananth@in.ibm.com>
    Cc: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
    Cc: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: linux-cris-kernel@axis.com
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-mips@linux-mips.org
    Cc: nios2-dev@lists.rocketboards.org
    Cc: linuxppc-dev@lists.ozlabs.org
    Cc: sparclinux@vger.kernel.org
    Cc: netdev@vger.kernel.org
    [lizf: Backported to 3.4: replace all module_free() to module_memfree()]
    Signed-off-by: Zefan Li <lizefan@huawei.com>
    Signed-off-by: Rui Xiang <rui.xiang@huawei.com>

--- arch/powerpc/net/bpf_jit_comp.c
+++ arch/powerpc/net/bpf_jit_comp.c
@@ -669,7 +669,7 @@ out:
 
 static void jit_free_defer(struct work_struct *arg)
 {
-	module_free(NULL, arg);
+	module_memfree(arg);
 }
 
 /* run from softirq, we must use a work_struct to call
--- arch/x86/net/bpf_jit_comp.c
+++ arch/x86/net/bpf_jit_comp.c
@@ -601,7 +601,7 @@ cond_branch:			f_offset = addrs[i + filter[i].jf] - addrs[i];
 				if (unlikely(proglen + ilen > oldproglen)) {
 					pr_err("bpb_jit_compile fatal error\n");
 					kfree(addrs);
-					module_free(NULL, image);
+					module_memfree(image);
 					return;
 				}
 				memcpy(image + proglen, temp, ilen);
@@ -653,7 +653,7 @@ out:
 
 static void jit_free_defer(struct work_struct *arg)
 {
-	module_free(NULL, arg);
+	module_memfree(arg);
 }
 
 /* run from softirq, we must use a work_struct to call
