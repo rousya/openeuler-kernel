246bbe61262d27085a6bad1388a9dba616d7d43e
Yufen Wang <wangyufen@huawei.com>
2016-06-24 17:28:19 +0800

    skbuff: Introduce skb_to_sgvec_nomark to map skb without mark new end
    mainline inclusion
    from: linux tags/v3.15-rc1~113^2~287^2~12
    target:    kernel 3.4
    category:  feture/IPv6
    
    -------------------------------
    commit 25a91d8d91911f84a03a039339b29537e7f1970d  upstream.
    
    As compared with skb_to_sgvec, skb_to_sgvec_nomark only map skb to given sglist
    without mark the sg which contain last skb data as the end. So the caller can
    mannipulate sg list as will when padding new data after the first call without
    calling sg_unmark_end to expend sg list.
    
    Signed-off-by: Fan Du <fan.du@windriver.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Yufen Wang <wangyufen@huawei.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -592,6 +592,9 @@ extern struct sk_buff *skb_realloc_headroom(struct sk_buff *skb,
 extern struct sk_buff *skb_copy_expand(const struct sk_buff *skb,
 				       int newheadroom, int newtailroom,
 				       gfp_t priority);
+extern int skb_to_sgvec_nomark(struct sk_buff *skb, struct scatterlist *sg,
+			       int offset, int len);
+
 extern int	       skb_to_sgvec(struct sk_buff *skb,
 				    struct scatterlist *sg, int offset,
 				    int len);
