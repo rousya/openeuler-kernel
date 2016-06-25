a83a9392b17b525011000463189930f8bc24b628
Rui Xiang <rui.xiang@huawei.com>
2016-06-25 17:24:00 +0800

    Revert "x86/ldt: Make modify_ldt synchronous"
    This reverts commit 0444d9dfacb4fa42005ca3afefe56b16f07c5192.
    
    Signed-off-by: Rui Xiang <rui.xiang@huawei.com>

--- arch/x86/kernel/step.c
+++ arch/x86/kernel/step.c
@@ -5,7 +5,6 @@
 #include <linux/mm.h>
 #include <linux/ptrace.h>
 #include <asm/desc.h>
-#include <asm/mmu_context.h>
 
 unsigned long convert_ip_to_linear(struct task_struct *child, struct pt_regs *regs)
 {
@@ -31,11 +30,10 @@ unsigned long convert_ip_to_linear(struct task_struct *child, struct pt_regs *re
 		seg &= ~7UL;
 
 		mutex_lock(&child->mm->context.lock);
-		if (unlikely(!child->mm->context.ldt ||
-			     (seg >> 3) >= child->mm->context.ldt->size))
+		if (unlikely((seg >> 3) >= child->mm->context.size))
 			addr = -1L; /* bogus selector, access would fault */
 		else {
-			desc = &child->mm->context.ldt->entries[seg];
+			desc = child->mm->context.ldt + seg;
 			base = get_desc_base(desc);
 
 			/* 16-bit code segment? */
