dfe8b58f9c561df080ab7e9643e815fbb8ee93a9
y00391025 <y00391025@notesmail.huawei.com>
2016-12-27 11:38:10 +0800

    tcp: take care of truncations done by sk_filter()
    Mainline inclusion
    from: linux v4.9
    target: kernel 3.4
    category:  CVE-2016-8645
    
    ----------------------
    
    commit ac6e780070e30e4c35bd395acfe9191e6268bdd3 upstream.
    
    With syzkaller help, Marco Grassi found a bug in TCP stack,
    crashing in tcp_collapse()
    
    Root cause is that sk_filter() can truncate the incoming skb,
    but TCP stack was not really expecting this to happen.
    It probably was expecting a simple DROP or ACCEPT behavior.
    
    We first need to make sure no part of TCP header could be removed.
    Then we need to adjust TCP_SKB_CB(skb)->end_seq
    
    Many thanks to syzkaller team and Marco for giving us a reproducer.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reported-by: Marco Grassi <marco.gra@gmail.com>
    Reported-by: Vladis Dronov <vdronov@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Jason Yan <yanaijie@huawei.com>

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -150,6 +150,7 @@ static inline unsigned int sk_filter_len(const struct sk_filter *fp)
 	return fp->len * sizeof(struct sock_filter) + sizeof(*fp);
 }
 
+int sk_filter_trim_cap(struct sock *sk, struct sk_buff *skb, unsigned int cap);
 extern int sk_filter(struct sock *sk, struct sk_buff *skb);
 extern unsigned int sk_run_filter(const struct sk_buff *skb,
 				  const struct sock_filter *filter);
