477e038118c417d0881e8f7b276308bb3626bf23
YueHaibing <yuehaibing@huawei.com>
2017-05-17 17:03:44 +0800

    tg3: don't clear stats while tg3_close
    mainline inclusion
    from mainline-v4.11
    commit 37a7fdf2892be124545ddaf99b85de576121476c
    category: bugfix
    bugzilla: 3145
    DTS: N/A
    CVE: N/A
    
    -----------------------------------
    
    Now tg3 NIC's stats will be cleared after ifdown/ifup. bond_get_stats traverse
    its salves to get statistics,cumulative the increment.If a tg3 NIC is added to
    bonding as a slave,ifdown/ifup will cause bonding's stats become tremendous value
    (ex.1638.3 PiB) because of negative increment.
    
    Fixes: 92feeabf3f67 ("tg3: Save stats across chip resets")
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Jason Yan <yanaijie@huawei.com>

--- drivers/net/ethernet/broadcom/tg3.c
+++ drivers/net/ethernet/broadcom/tg3.c
@@ -10238,10 +10238,6 @@ static int tg3_close(struct net_device *dev)
 
 	tg3_ints_fini(tp);
 
-	/* Clear stats across close / open calls */
-	memset(&tp->net_stats_prev, 0, sizeof(tp->net_stats_prev));
-	memset(&tp->estats_prev, 0, sizeof(tp->estats_prev));
-
 	tg3_napi_fini(tp);
 
 	tg3_free_consistent(tp);
